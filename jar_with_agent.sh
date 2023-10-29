ed178a8635b056359fcec6f33e0a4c6b706c5859
Helly Guo <hellyguo@foxmail.com>
2023-10-29 23:02:30 +0800

    test: shell

--- jar_with_agent.sh
+++ jar_with_agent.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 CURR_DIR=$(pwd)
-LIB_PATH=$CURR_DIR/build/linux/x86_64/debug
-#LIB_PATH=$CURR_DIR/build/linux/x86_64/release
+DEBUG_LIB_PATH=$CURR_DIR/build/linux/x86_64/debug
+RELEASE_LIB_PATH=$CURR_DIR/build/linux/x86_64/release
 
 cd jar_test || exit 1
 
 export JAVA_HOME="$JAVA_HOME8_IBM"
 export PATH="$JAVA_HOME/bin":"$PATH"
-export LD_LIBRARY_PATH="$LIB_PATH":"$LD_LIBRARY_PATH"
 export JAVA_OPTS="-Xmx2G -Xms2G"
+
+OLD_LD_PATH=$LD_LIBRARY_PATH
 
 echo normal call
 time java $JAVA_OPTS -jar tiny-rules-sample.jar
 
-echo call with agent
-time java $JAVA_OPTS -agentlib:j9objrecord -jar tiny-rules-sample.jar
+echo call with agent, debug
+time LD_LIBRARY_PATH=$DEBUG_LIB_PATH:$OLD_LD_PATH java $JAVA_OPTS -agentlib:j9objrecord -jar tiny-rules-sample.jar
+
+echo call with agent, debug
+time LD_LIBRARY_PATH=$RELEASE_LIB_PATH:$OLD_LD_PATH java $JAVA_OPTS -agentlib:j9objrecord -jar tiny-rules-sample.jar
+
 #time valgrind java $JAVA_OPTS -agentlib:j9objrecord -jar tiny-rules-sample.jar
--- test_java_calc_with_agent.sh
+++ test_java_calc_with_agent.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
+function compile(){
+    xmake config -m $1
+    if xmake
+    then
+        echo compile successfully
+    else
+        echo compile failed
+        exit 1
+    fi
+}
+
 if [ -e "$HOME/.bashrc" ]; then
     source "$HOME/.bashrc"
 fi
 
-#DEBUG=0
-DEBUG=1
 CURR_DIR=$(pwd)
 
 DEBUG_BINARY_PATH=$CURR_DIR"/build/linux/x86_64/debug"
 RELEASE_BINARY_PATH=$CURR_DIR"/build/linux/x86_64/release"
 
-if xmake
-then
-    echo compile successfully
-else
-    echo compile failed
-    exit 1
-fi
-
-if [ "$DEBUG" -eq "1" ]; then
-    LIB_PATH=$DEBUG_BINARY_PATH
-else
-    LIB_PATH=$RELEASE_BINARY_PATH
-fi
+compile debug
+compile release
 
 export JAVA_HOME="$JAVA_HOME8_IBM"
 export PATH="$JAVA_HOME/bin":"$PATH"
-export LD_LIBRARY_PATH="$LIB_PATH":"$LD_LIBRARY_PATH"
 
 javac -sourcepath java_src -cp java_classes:"$CLASSPATH" -d java_classes java_src/calc/A.java
 javac -sourcepath java_src -cp java_classes:"$CLASSPATH" -d java_classes java_src/calc/B.java
@@ -36,6 +33,10 @@ javac -sourcepath java_src -cp java_classes:"$CLASSPATH" -d java_classes java_sr
 echo normal call
 time java -cp java_classes:"$CLASSPATH" calc.A
 
-echo call with agent
-time java -agentlib:j9objrecord -cp java_classes:"$CLASSPATH" calc.A
+OLD_LD_PATH=$LD_LIBRARY_PATH
 
+echo call with agent, debug
+time LD_LIBRARY_PATH=$DEBUG_BINARY_PATH:$OLD_LD_PATH java -agentlib:j9objrecord -cp java_classes:"$CLASSPATH" calc.A
+
+echo call with agent, release
+time LD_LIBRARY_PATH=$RELEASE_BINARY_PATH:$OLD_LD_PATH java -agentlib:j9objrecord -cp java_classes:"$CLASSPATH" calc.A
