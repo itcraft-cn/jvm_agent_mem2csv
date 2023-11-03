542ac9339903949ee1c0f42943e6bb6fe750bc11
Helly Guo <hellyguo@foxmail.com>
2023-11-03 15:30:54 +0800

    build: compile by gcc directly

--- compile2.sh
+++ compile2.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# compile/link by gcc direct
-gcc -DJ9 -c -O3 -fPIC -I$JAVA_HOME8_IBM/include -o main.o src/main.c
-gcc -shared -fPIC -o libj2mem2csv.so main.o
-gcc -c -O3 -fPIC -I$JAVA_HOME8/include -I$JAVA_HOME8/include/linux -o main.o src/main.c
-gcc -shared -fPIC -o libhsmem2csv.so main.o
-rm -rf main.o
+# compile/link by gcc directly
+if [ -e build ]; then
+    echo "dir [build] exists, creating is skipped"
+else
+    mkdir build
+fi
+
+gcc -DJ9 -c -O3 -fPIC -I$JAVA_HOME8_IBM/include -o build/main.o src/main.c
+gcc -shared -fPIC -o build/libj2mem2csv.so build/main.o
+gcc -c -O3 -fPIC -I$JAVA_HOME8/include -I$JAVA_HOME8/include/linux -o build/main.o src/main.c
+gcc -shared -fPIC -o build/libhsmem2csv.so build/main.o
+rm -rf build/main.o
