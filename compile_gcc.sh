85dc061156f2b07536cb2beff1b84f264425abba
Helly Guo <hellyguo@foxmail.com>
2023-12-01 11:40:34 +0800

    build: rename

--- compile_gcc.sh
+++ compile_gcc.sh
@@ -8,7 +8,7 @@ else
 fi
 
 gcc -DJ9 -c -O3 -fPIC -I$JAVA_HOME8_IBM/include -o build/main.o src/main.c
-gcc -shared -fPIC -o build/libj2mem2csv.so build/main.o
+gcc -shared -fPIC -o build/libj9mem2csv.so build/main.o
 gcc -c -O3 -fPIC -I$JAVA_HOME8/include -I$JAVA_HOME8/include/linux -o build/main.o src/main.c
 gcc -shared -fPIC -o build/libhsmem2csv.so build/main.o
 rm -rf build/main.o
